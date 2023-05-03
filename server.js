38f12314468ef0afa84cd2bc9593db7a86513ac3
Quoc Tan <34764783+quoctann@users.noreply.github.com>
2023-05-03 22:33:01 +0700

    Update server.js to see how pipeline is triggered

--- server.js
+++ server.js
@@ -172,5 +172,6 @@ app.use(express.static(__dirname + '/views'));
 
 let port = process.env.PORT || 8080
 app.listen(port, function () {
+  console.log("Hello world");
   console.log("To view your app, open this link in your browser: http://localhost:" + port);
 });
